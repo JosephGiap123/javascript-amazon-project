0737835da35272eee0a75909b88d6a015cd79750
Joseph Giap <145407086+JosephGiap123@users.noreply.github.com>
2024-11-12 16:44:07 -0600

    made dates and delivery options dynamic

--- data/cart.js
+++ data/cart.js
@@ -7,7 +7,7 @@
 1. add type = "module" lets files get vars out of other files
 2. export
 3. import */
-export const cart = [];
+export let cart = JSON.parse(localStorage.getItem('cart')) || []; //its still a string.
 
 export function addToCart(productId){
 	const quantityAdd = Number(document.querySelector(`.js-select-${productId}-value`).value);
@@ -25,7 +25,24 @@ export function addToCart(productId){
 	else{
 		cart.push({
 			productId: productId,
-			quantity: quantityAdd
+			quantity: quantityAdd,
+			deliveryOptionId: '1'
 		});
 	}
+	saveToStorage(cart);
 };
+
+function saveToStorage(){
+	localStorage.setItem('cart', JSON.stringify(cart)); //2 parameter, name, data wanted to save, can only save strings.
+}
+
+export function removeFromCart(productId){
+	const newCart = [];
+	cart.forEach((cartItem) => {
+		if(cartItem.productId !== productId){
+			newCart.push(cartItem);
+		}
+	});
+	cart = newCart;
+	saveToStorage(cart);
+}
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,6 @@
 import {cart as myCart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
+import { formatCurrency } from './utils/money.js';
 
 let productsHTML = '';
 
@@ -23,7 +24,7 @@ products.forEach((productObject)=> {
 			</div>
 
 			<div class="product-price">
-				$${(productObject.priceCents/100).toFixed(2)}
+				$${formatCurrency(productObject.priceCents)}
 			</div>
 
 			<div class="product-quantity-container">
